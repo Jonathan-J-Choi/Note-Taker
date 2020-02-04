d7cbaa15d38bc226444a77c45513e978daea5f18
Jonathan Choi <Jonathan.Junghoon.Choi@gmail.Com>
2020-02-04 18:38:18 -0500

    basic server setup

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+// Express application
+var app = express();
+
+// Initial port
+var PORT = process.env.PORT || 8080;
+
+// Express app data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Router
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+})
