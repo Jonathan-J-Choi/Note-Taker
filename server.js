6fb254db98e774bc3c70f8b7d9de474e6f2a6b9b
Jonathan Choi <Jonathan.Junghoon.Choi@gmail.Com>
2020-02-05 10:13:49 -0500

    notes saving working, delete not working yet

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-// Dependencies
+// Dependencies //
 var express = require("express");
 
-// Express application
+// Express application //
 var app = express();
 
-// Initial port
+// Initial port //
 var PORT = process.env.PORT || 8080;
 
-// Express app data parsing
+// Express app data parsing //
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// Router
-// require("./routes/apiRoutes")(app);
+// Router //
+require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// Listener
+// Listener //
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 })
